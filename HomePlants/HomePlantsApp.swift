ca0ebcc321f9987764b620a83027dfbbe8d2ac99
Christian Menschel <christian@tapwork.de>
2021-03-30 19:20:24 +0200

    Initial Commit

---
+++ HomePlants/HomePlantsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomePlantsApp.swift
+//  HomePlants
+//
+//  Created by Christian Menschel on 30.03.21.
+//
+
+import SwiftUI
+
+@main
+struct HomePlantsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
